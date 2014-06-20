a90843c9e34100ff960a2fa8138181b871767cf5
Matthew Duffy <mattduffy@gmail.com>
2014-06-20 14:52:32 -0700

    Starting to use should.js for unit testing.

--- test/test.js
+++ test/test.js
@@ -4,15 +4,20 @@ var os = require('os'),
     data = require('../data/mockdata').data;
 
 
-console.log("++++++++++ testing the app ++++++++++");
-console.log("does the exported data exist?");
-poo = should.exist(data, "data exported from data file");
+console.log("++++++++++ testing the app ++++++++++\n");
+console.log("\t testing successful data connection.");
 
-console.log("++++++++++ testing the poo ++++++++++");
-console.log(poo);
-console.log("++++++++++ testing the poo ++++++++++");
+console.log("\t>>>>> Does the exported data exist?");
+var is_data_there = should.exist(data, "data seems to not be exported from data file");
+console.log(is_data_there);
+if (is_data_there != null && typeof is_data_there == 'object') {
+    console.log(is_data_there);
+}
+
+
 
 //console.log(data.accounts);
 is_mock = should(data.status).equal('muck', data.status);
-console.log(is_mock, " mock data is mock");
-console.log(data.getLiquidBalanceTtl(data.accounts));
+//console.log(is_mock, " mock data is mock");
+console.log("\n\t++++++++++ mock liquid balances sum: %d\n", data.getLiquidBalanceTtl(data.accounts));
+
