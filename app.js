6b277a82cb92abc1b0973d1fba8424f7c1f6a1e4
Matthew Duffy <mattduffy@gmail.com>
2014-07-03 13:22:09 -0700

    Add some more scaffolding to the project:
    - add new jquery javascript library
    
    - configure express middleware for file uploads to the root level
      uploads directory

--- app.js
+++ app.js
@@ -13,9 +13,12 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('view options', {layout: true});
+//app.locals.pretty = true;
 
 app.use(favicon());
 app.use(logger('dev'));
+app.use(bodyParser({keepExtensions: true, uploadDir: './uploads'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
@@ -24,10 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-/// example
-app.use('/list', routes);
-app.use('/list/json', routes);
-
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -35,8 +34,8 @@ app.use(function(req, res, next) {
     next(err);
 });
 
-/// error handlers
 
+/// error handlers
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -58,6 +57,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-
 
 module.exports = app;
